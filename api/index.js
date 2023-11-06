f3cba44fc751eef94573bdec0c95fddc57a94f9d
Ajaya Prajapati <ajz.prz@gmail.com>
2023-11-06 03:47:24 -0500

    connection to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('Server listening on port 3000')
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database connected Successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
