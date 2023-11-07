2fb463b6c3f70397737b0ec119c5d841790b7b3b
Ajaya Prajapati <ajz.prz@gmail.com>
2023-11-07 08:20:54 -0500

    added redux presist and login ui and routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,11 +34,14 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(errorHandler(404, "User not found"));
     }
+    if (!password) {
+      return next(errorHandler(401, "Fields cannot be empty"));
+    }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now + 3600000); //1 hr
+    const expiryDate = new Date(Date.now() + 3600000); //1 hr
     res
       .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(201)
